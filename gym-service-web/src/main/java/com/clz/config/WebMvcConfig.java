1de24ad66988a3db5e9a815589660cb030249bae
cheery6103 <3141988275@qq.com>
2025-10-28 11:30:00 +0800

    添加WebMvcConfigurer解决跨域问题

---
+++ gym-service-web/src/main/java/com/clz/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.clz.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 解决跨域问题
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    /**
+     * 跨域配置
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
