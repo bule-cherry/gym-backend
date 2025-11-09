40de6be3e8d8186312d8536d10d39b2d50d61b0e
13738017354 <15913042+bulecherry@user.noreply.gitee.com>
2025-11-09 14:54:13 +0800

    密码的重置和修改

--- gym-service-web/src/main/java/com/clz/web/home/controller/HomeController.java
+++ gym-service-web/src/main/java/com/clz/web/home/controller/HomeController.java
@@ -1,20 +1,23 @@
 package com.clz.web.home.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.clz.utils.ResultUtils;
 import com.clz.utils.ResultVo;
 import com.clz.web.goods_order.service.GoodsOrderService;
 import com.clz.web.home.entity.Echart;
 import com.clz.web.home.entity.EchartItem;
+import com.clz.web.home.entity.ResetPassword;
 import com.clz.web.home.entity.TotalCount;
 import com.clz.web.material.service.MaterialService;
+import com.clz.web.member.entity.Member;
 import com.clz.web.member.service.MemberService;
 import com.clz.web.suggest.entity.Suggest;
 import com.clz.web.suggest.service.SuggestService;
+import com.clz.web.sys_user.entity.SysUser;
 import com.clz.web.sys_user.service.SysUserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -80,5 +83,46 @@ public class HomeController {
     public ResultVo getHotCourse(){
         List<EchartItem> echartItems = goodsOrderService.hotCourse();
         return ResultUtils.success("查询成功",echartItems);
+    }
+
+    @PostMapping("/resetPassword")
+    public ResultVo resetPassword( @RequestBody ResetPassword resetPassword) {
+        if("1".equals(resetPassword.getUserType())){//会员
+            LambdaUpdateWrapper<Member> query = new LambdaUpdateWrapper<Member>()
+                    .eq(Member::getMemberId, resetPassword.getUserId()).set(Member::getPassword, DigestUtils.md5DigestAsHex("123456".getBytes()));
+            memberService.update(query);
+            return ResultUtils.success("重置密码成功!");
+        } else if ("2".equals(resetPassword.getUserType())) {
+            LambdaUpdateWrapper<SysUser> query = new LambdaUpdateWrapper<SysUser>()
+                    .eq(SysUser::getUserId, resetPassword.getUserId()).set(SysUser::getPassword, DigestUtils.md5DigestAsHex("123456".getBytes()));
+            userService.update(query);
+            return ResultUtils.success("重置密码成功!");
+        }else{
+            return ResultUtils.error("用户类型错误");
+        }
+    }
+    @PostMapping("/updatePassword")
+    public ResultVo updatePassword(@RequestBody ResetPassword resetPassword) {
+        if("1".equals(resetPassword.getUserType())){//会员
+            Member member = memberService.getById(resetPassword.getUserId());
+            if(!member.getPassword().equals(DigestUtils.md5DigestAsHex(resetPassword.getOldPassword().getBytes()))){
+                return ResultUtils.error("原密码不正确");
+            }
+            LambdaUpdateWrapper<Member> query = new LambdaUpdateWrapper<Member>()
+                    .eq(Member::getMemberId, resetPassword.getUserId()).set(Member::getPassword, DigestUtils.md5DigestAsHex(resetPassword.getPassword().getBytes()));
+            memberService.update(query);
+            return ResultUtils.success("重置密码成功!");
+        } else if ("2".equals(resetPassword.getUserType())) {
+            SysUser user = userService.getById(resetPassword.getUserId());
+            if(!user.getPassword().equals(DigestUtils.md5DigestAsHex(resetPassword.getOldPassword().getBytes()))){
+                return ResultUtils.error("原密码不正确");
+            }
+            LambdaUpdateWrapper<SysUser> query = new LambdaUpdateWrapper<SysUser>()
+                    .eq(SysUser::getUserId, resetPassword.getUserId()).set(SysUser::getPassword, DigestUtils.md5DigestAsHex(resetPassword.getPassword().getBytes()));
+            userService.update(query);
+            return ResultUtils.success("重置密码成功!");
+        }else{
+            return ResultUtils.error("用户类型错误");
+        }
     }
 }
